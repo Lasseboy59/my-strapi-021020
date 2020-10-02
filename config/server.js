2bd93e024eb7a7f5ff6ae0bf62b0aa31c978fa06
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-10-02 11:42:54 +0300

    initial commit - added job-description

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '86971d97b985128ccc27619e767a6188'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9c611f8b-b633-4ae2-a778-a63016270f38'
+};
